e253c6de4fffc7fbad461072d211e16366a43c24
CodeCurator_Aryan <aryanaryan13115@gmail.com>
2026-08-03 08:44:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (98.58%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int start = 0 , end = piles.size() - 1;
-        long a1 = 0 , a2 = 0;
-
-        while(start < end) {
-            int x = max(piles[start] , piles[end]);
-            a1 += x;
-            a2 += ((piles[start] + piles[end]) - x);
-            start++;
-            end--;
-        }
-        return (a1 > a2);
+        return true;
     }
 };
