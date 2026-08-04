5020c502d2d7157e9f7cfc2de4f9d27f82376a81
CodeCurator_Aryan <aryanaryan13115@gmail.com>
2026-08-04 16:46:11 +0530

    Time: 0 ms (100%), Space: 11 MB (45.44%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,21 +12,39 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
+        // vector<int> ans;
+        // if(!root) return ans;
+        // stack<TreeNode*>st1 , st2;
+        // TreeNode* curr = root;
+        // st1.push(curr);
+        // while(!st1.empty()) {
+        //     curr = st1.top();
+        //     st1.pop();
+        //     st2.push(curr);
+        //     if(curr -> left) st1.push(curr -> left);
+        //     if(curr -> right) st1.push(curr -> right);
+        // }
+        // while(!st2.empty()) {
+        //     ans.push_back(st2.top() -> val);
+        //     st2.pop();
+        // }
+
         vector<int> ans;
         if(!root) return ans;
-        stack<TreeNode*>st1 , st2;
-        TreeNode* curr = root;
-        st1.push(curr);
-        while(!st1.empty()) {
-            curr = st1.top();
-            st1.pop();
-            st2.push(curr);
-            if(curr -> left) st1.push(curr -> left);
-            if(curr -> right) st1.push(curr -> right);
-        }
-        while(!st2.empty()) {
-            ans.push_back(st2.top() -> val);
-            st2.pop();
+        stack<TreeNode*> st;
+        TreeNode* curr = root , *last = nullptr;
+        while(curr || !st.empty()) {
+            while(curr) {
+                st.push(curr);
+                curr = curr -> left;
+            }
+            TreeNode* node = st.top();
+            if(node -> right && last != node -> right) curr = node -> right;
+            else {
+                ans.push_back(node -> val);
+                last = node;
+                st.pop();
+            }
         }
         return ans;
     }
