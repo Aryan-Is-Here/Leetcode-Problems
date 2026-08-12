341dfa49c23c0135e0e6ef3b2f1f05d01d391c75
CodeCurator_Aryan <aryanaryan13115@gmail.com>
2026-08-12 09:43:58 +0530

    Time: 53 ms (92.86%), Space: 149.4 MB (56.74%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -18,24 +18,24 @@ public:
             }
             high++;
         }
-        ans = max(ans , high - low);
-        unordered_map<int , int> check1;
-        low = nums.size() - 1 , high = nums.size() - 1;
-        int ans1 = 0;
-        while(low >= 0) {
-            check1[nums[low]]++;
-            if(check1[nums[low]] > k) {
-                ans1 = max(ans1 , high - low);
-                while(nums[high] != nums[low]) {
-                    check1[nums[high]]--;
-                    high--;
-                }
-                check1[nums[high]]--;
-                high--;
-            }
-            low--;
-        }
-        ans1 = max(ans1 , high - low);
-        return max(ans , ans1);
+        return max(ans , high - low);
+        // unordered_map<int , int> check1;
+        // low = nums.size() - 1 , high = nums.size() - 1;
+        // int ans1 = 0;
+        // while(low >= 0) {
+        //     check1[nums[low]]++;
+        //     if(check1[nums[low]] > k) {
+        //         ans1 = max(ans1 , high - low);
+        //         while(nums[high] != nums[low]) {
+        //             check1[nums[high]]--;
+        //             high--;
+        //         }
+        //         check1[nums[high]]--;
+        //         high--;
+        //     }
+        //     low--;
+        // }
+        // ans1 = max(ans1 , high - low);
+        // return max(ans , ans1);
     }
 };
