ff70b64c213c381e2216401a3c0dffe6d120cc30
CodeCurator_Aryan <aryanaryan13115@gmail.com>
2026-09-02 10:39:35 +0530

    Time: 594 ms (5.05%), Space: 192.4 MB (5.25%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    void dfs(TreeNode * root , long long sum , int t , unordered_map<long long , int>mp) {
+        if(!root) return;
+        sum += root -> val;
+        if(mp.find(sum - t) != mp.end()) count += mp[sum - t];
+        mp[sum]++;
+        dfs(root -> left , sum , t , mp);
+        dfs(root -> right , sum , t , mp);
+        mp[sum]--;
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long long , int> mp;
+        mp[0] = 1;
+        dfs(root , 0 , targetSum , mp);
+        return count;
+    }
+};
