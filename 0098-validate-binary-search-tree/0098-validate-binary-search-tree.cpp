7fdb49b89668417661e87fd498b169854887f1b1
CodeCurator_Aryan <aryanaryan13115@gmail.com>
2026-09-11 16:39:38 +0530

    Time: 0 ms (100%), Space: 21.9 MB (74.88%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,33 +11,17 @@
  */
 class Solution {
 public:
+    bool ans = true;
+    TreeNode *prev = nullptr;
+    void dfs(TreeNode *root) {
+        if(!root || !ans) return;
+        dfs(root -> left);
+        if(prev && prev -> val >= root -> val) ans = false;
+        prev = root;
+        dfs(root -> right);
+    }
     bool isValidBST(TreeNode* root) {
-        if(!root) return true;
-        TreeNode * prev1;
-        TreeNode* prev2 = nullptr;
-        TreeNode * curr = root;
-        bool ans = true;
-        while(curr) {
-            if(!curr -> left) {
-                if(prev2 && curr -> val <= prev2 -> val) ans = false;
-                prev2 = curr;
-                curr = curr -> right;
-            }
-            else {
-                prev1 = curr -> left;
-                while(prev1 -> right && prev1 -> right != curr) prev1 = prev1 -> right;
-                if(!prev1 -> right) {
-                    prev1 -> right = curr;
-                    curr = curr -> left;
-                }
-                else {
-                    prev1 -> right = nullptr;
-                    if(prev2 && curr -> val <= prev2 -> val) ans = false;
-                    prev2 = curr;
-                    curr = curr -> right;
-                }
-            }
-        }
+        dfs(root);
         return ans;
     }
 };
